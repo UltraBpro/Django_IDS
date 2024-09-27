8b631c53958d51c3a062c3389467739074e63fcd
TrueSuperiorBpro <b3bpro123@gmail.com>
2024-09-27 10:10:45 +0700

    test file upload

--- IDSWeb/rules.py
+++ IDSWeb/rules.py
@@ -23,6 +23,13 @@ def apply_rules(log_entry):
             "attack_type": "CSRF Attack",
             "severity": "Medium"
         })
+    if file_upload_vulnerability(log_entry):
+        print("File Upload Vulnerability")
+        alerts.append({
+            "log_entry": log_entry,
+            "attack_type": "File Upload Vulnerability",
+            "severity": "High"
+        })
     # Thêm các quy tắc khác ở đây
     return alerts
 
@@ -60,5 +67,32 @@ def xss_attack(log_entry):
 def csrf_attack(log_entry):
     pattern = r"(Forbidden \(CSRF cookie|token (missing|incorrect|not set).*?\)|CSRF verification failed)"
     return bool(re.search(pattern, log_entry, re.IGNORECASE))
+
+def file_upload_vulnerability(log_entry):
+    dangerous_extensions = [
+        r"\.php",
+        r"\.phtml",
+        r"\.php3",
+        r"\.php4",
+        r"\.php5",
+        r"\.phps",
+        r"\.asp",
+        r"\.aspx",
+        r"\.jsp",
+        r"\.jspx",
+        r"\.exe",
+        r"\.dll",
+        r"\.bat",
+        r"\.cmd",
+        r"\.sh",
+        r"\.cgi",
+        r"\.pl",
+        r"\.py",
+        r"\.rb",
+    ]
+    
+    pattern = r'"\w+\s+.*?/media/.*?({})'.format('|'.join(dangerous_extensions))
+    return bool(re.search(pattern, log_entry, re.IGNORECASE))
+
 
 # Thêm các quy tắc khác tùy theo nhu cầu
